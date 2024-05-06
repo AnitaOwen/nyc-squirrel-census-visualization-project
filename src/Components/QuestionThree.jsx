7e3b0e607815d29dee4698f1e08768147d089d9b
Jennifer <jenniferpeterson@pursuit.org>
2024-05-06 12:49:59 -0400

    updated the question 3 component

--- src/Components/QuestionThree.jsx
+++ src/Components/QuestionThree.jsx
@@ -1,5 +1,53 @@
 const QuestionThree = () => {
-  return <div>QuestionThree</div>;
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+  return (
+    <div>
+      <div>
+        <p>
+          My friends can either be very chatty or quiet. What sounds did you
+          hear from the squirrel you just saw?
+        </p>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="quiet">
+              <input
+                type="radio"
+                id="quiet"
+                name="sound"
+                value="quiet"
+                // onChange={handleChange}
+                required
+              />
+              Quiet
+            </label>
+            <label htmlFor="kukking">
+              <input
+                type="radio"
+                id="kukking"
+                name="sound"
+                value="kukking"
+                // onChange={handleChange}
+              />
+              Chirpy/Kukking
+            </label>
+            <label htmlFor="quaaing">
+              <input
+                type="radio"
+                id="quaaing"
+                name="sound"
+                value="quaaing"
+                // onChange={handleChange}
+              />
+              Quacking/Quaaing
+            </label>
+            <button type="submit">Next Question</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default QuestionThree;
