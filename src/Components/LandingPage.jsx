f8d00fd2dd68dc492e130f1357e0d9c745e0dd2e
Haiyah Perez <haiyahperez@pursuit.org>
2024-05-06 13:14:18 -0400

    landing

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,5 +1,10 @@
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return 
+  <div>
+  <div><h2>Squirrel Scouts of Central Park</h2></div>
+  <div><h3>Hi friend! My name is Rosie and I am a squirrel scout. After a day of selling cookies, my friends and I got chased by a big dog. Now I can't find them. Can you help me find my friends?</h3></div>
+  </div>
+
 };
 
 export default LandingPage;
