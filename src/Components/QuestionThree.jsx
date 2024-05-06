75c8b0b72d6451d035b65451c2b8f030b67c96fb
Jennifer <jenniferpeterson@pursuit.org>
2024-05-06 16:42:45 -0400

    able to access the data in the QUESTION 3 Component

--- src/Components/QuestionThree.jsx
+++ src/Components/QuestionThree.jsx
@@ -1,71 +1,88 @@
-import { useState } from "react";
-const QuestionThree = ({answer,  setAnswer }) => {
-  const [sound, setSound] = useState("");
-  const [formSubmitted, setFormSubmitted] = useState(false);
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+const API = import.meta.env.VITE_API_URL;
+
+const QuestionThree = ({ answer, setAnswer }) => {
+  // const [sound, setSound] = useState("");
+  const [toggleForm, setToggleForm] = useState(true);
+  const [data, setData] = useState([]);
 
   function handleSubmit(event) {
     event.preventDefault();
-    setFormSubmitted(true);
+    setToggleForm(false);
+    fetch(`${API}?${answer.sound}=false`)
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error(error));
   }
 
   function handleChange(event) {
-    setSound(event.target.value);
+    setAnswer({ ...answer, [event.target.name]: event.target.value });
   }
+
+  useEffect(() => {
+    console.log(data); // This will log whenever data changes
+  }, [data]);
+
   return (
     <div>
-      <div>
-        <p>
-          My friends can either be very chatty or quiet. What sounds did you
-          hear from the squirrel you just saw?
-        </p>
-        <form onSubmit={handleSubmit}>
-          <div>
+      {toggleForm && (
+        <div>
+          <p>
+            My friends can either be very chatty or quiet. What sounds did you
+            hear from the squirrel you just saw?
+          </p>
+          <form onSubmit={handleSubmit}>
             <div>
-              <label htmlFor="quiet">
+              <label htmlFor="indifferent">
                 <input
                   type="radio"
-                  id="quiet"
+                  id="indifferent"
                   name="sound"
-                  value="quiet"
+                  value="indifferent"
                   onChange={handleChange}
                   required
                 />
                 Quiet
               </label>
-            </div>
 
-            <div>
-              <label htmlFor="kukking">
+              <label htmlFor="kuks">
                 <input
                   type="radio"
-                  id="kukking"
+                  id="kuks"
                   name="sound"
-                  value="kukking"
+                  value="kuks"
                   onChange={handleChange}
                 />
                 Chirpy/Kukking
               </label>
-            </div>
 
-            <div>
-              <label htmlFor="quaaing">
-                <input
-                  type="radio"
-                  id="quaaing"
-                  name="sound"
-                  value="quaaing"
-                  onChange={handleChange}
-                />
-                Quacking/Quaaing
-              </label>
-            </div>
-            <div>
+              <div>
+                <label htmlFor="quaas">
+                  <input
+                    type="radio"
+                    id="quaas"
+                    name="sound"
+                    value="quaas"
+                    onChange={handleChange}
+                  />
+                  Quacking/Quaaing
+                </label>
+              </div>
+
               <button type="submit">Submit</button>
             </div>
-          </div>
-        </form>
-        {formSubmitted && <button>Next Question</button>}
-      </div>
+          </form>
+        </div>
+      )}
+      {!toggleForm && (
+        <div>
+          <p>CALCULATIONS</p>
+          <Link to={`/question4`}>
+            <button>Next Question</button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
