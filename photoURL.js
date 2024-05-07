e4dcb33cfc0d418fd6b08a3588c5967056cf1a57
Jennifer <jenniferpeterson@pursuit.org>
2024-05-06 23:29:59 -0400

    added an array of photos to randomize

---
+++ photoURL.js
@@ -0,0 +1,16 @@
+const urls = [
+  "https://res.cloudinary.com/dluexpyt4/image/upload/c_crop,ar_16:9/v1715038036/Squirrel%20Scouts%20NYC/geranimo-bKhETeDV1WM-unsplash_m1obne.jpg",
+  "https://res.cloudinary.com/dluexpyt4/image/upload/c_crop,ar_16:9/v1715038040/Squirrel%20Scouts%20NYC/remi-skatulski-7d2tOLLL5gE-unsplash_ldakid.jpg",
+  "https://res.cloudinary.com/dluexpyt4/image/upload/c_crop,ar_16:9/v1715038043/Squirrel%20Scouts%20NYC/ilnur-kalimullin-D3fLgDc9uOQ-unsplash_sdawfn.jpg",
+  "https://res.cloudinary.com/dluexpyt4/image/upload/c_crop,ar_16:9/v1715038069/Squirrel%20Scouts%20NYC/joakim-honkasalo-3Xdff7Amc8k-unsplash_szhudn.jpg",
+  "https://res.cloudinary.com/dluexpyt4/image/upload/v1715038110/Squirrel%20Scouts%20NYC/jorge-fernandez-Yoa8SX2Ofs8-unsplash_obqmbk.jpg",
+  "https://res.cloudinary.com/dluexpyt4/image/upload/c_crop,ar_16:9/v1715038154/Squirrel%20Scouts%20NYC/camerauthor-photos-DoQWhSLs5FA-unsplash_l8zckq.jpg",
+  "https://res.cloudinary.com/dluexpyt4/image/upload/v1715038171/Squirrel%20Scouts%20NYC/alex-ranney-CwVS0PoNm8o-unsplash_fxyhxb.jpg",
+  "https://res.cloudinary.com/dluexpyt4/image/upload/v1715038171/Squirrel%20Scouts%20NYC/alex-ranney-CwVS0PoNm8o-unsplash_fxyhxb.jpg",
+  "https://res.cloudinary.com/dluexpyt4/image/upload/v1715038171/Squirrel%20Scouts%20NYC/alex-ranney-CwVS0PoNm8o-unsplash_fxyhxb.jpg",
+  "https://res.cloudinary.com/dluexpyt4/image/upload/v1715044366/Squirrel%20Scouts%20NYC/andy-willis-3BBtFjGXbE8-unsplash_1_nxqdmn.jpg",
+  "https://res.cloudinary.com/dluexpyt4/image/upload/c_crop,ar_16:9/v1715044396/Squirrel%20Scouts%20NYC/alex-west-9bFb1u8tOwA-unsplash_wcmwuv.jpg",
+  "https://res.cloudinary.com/dluexpyt4/image/upload/c_crop,ar_16:9/v1715044427/Squirrel%20Scouts%20NYC/lukas-skoe-6_wsDWOUNhA-unsplash_jsl3d8.jpg",
+];
+
+module.exports = urls;
